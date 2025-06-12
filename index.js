7493485ab0a2fd7c5045fa8ac2f247cb0b327668
kurui <kuruijoshua@gmail.com>
2025-06-12 12:27:41 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+// creating a variable to hold the result of multiplication
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+//printing the result of multiplication
+console.log(multiply);
+//...........................
+//variable to generate a random number greater than 0
+
+let random = Math.floor(Math.random() * 10) + 1;
+//printing the random number
+console.log(random);
+//...........................
+//variable to set an equation that will return a remainder
+let num3= 13;
+let num4 = 9;
+let mod=num3%num4;
+//printing the result of the modulus operation
+console.log(mod);
+//...........................
+//variable that finds the highest number in a set
+let num=(1,5,7,20);
+let max = Math.max(num);
+//printing the highest number
+console.log(max);
